84e2d1a96c18d580844b631d374bfc62833f6c20
Adrigamer2950 <adrigamer2950@gmail.com>
2024-03-17 14:13:32 +0100

    Removed unused methods and constructors

--- src/main/java/me/adrigamer2950/premiumtags/objects/Pagination.java
+++ src/main/java/me/adrigamer2950/premiumtags/objects/Pagination.java
@@ -1,37 +1,19 @@
 package me.adrigamer2950.premiumtags.objects;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Pagination<T> extends ArrayList<T> {
 
     private final int pageSize;
 
-    public Pagination(int pageSize) {
-        this(pageSize, new ArrayList<T>());
-    }
-
-    @SafeVarargs
-    public Pagination(int pageSize, T... objects) {
-        this(pageSize, Arrays.asList(objects));
-    }
-
     public Pagination(int pageSize, List<T> objects) {
         this.pageSize = pageSize;
         addAll(objects);
     }
 
-    public int pageSize() {
-        return pageSize;
-    }
-
     public int totalPages() {
         return (int) Math.ceil((double) size() / pageSize);
-    }
-
-    public boolean exists(int page) {
-        return !(page < 0) && page < totalPages();
     }
 
     public List<T> getPage(int page) {
