88de6023b7e8136ff66e06f95c2d90b4af76da20
Adrigamer2950 <adrigamer2950@gmail.com>
2024-09-06 04:36:02 +0200

    feat: Add base Tag class

---
+++ src/main/java/me/adrigamer2950/premiumtags/objects/Tag.java
@@ -0,0 +1,12 @@
+package me.adrigamer2950.premiumtags.objects;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Tag {
+
+    private final String id;
+    private final String tag;
+}
