8ffd8756784c8343bfb71ec7c7add589c0453c79
Adrigamer2950 <adrigamer2950@gmail.com>
2024-09-20 19:06:32 +0200

    feat: Add Permissions constants

---
+++ src/main/java/me/adrigamer2950/premiumtags/util/Permissions.java
@@ -0,0 +1,7 @@
+package me.adrigamer2950.premiumtags.util;
+
+public class Permissions {
+
+    public static final String ALL = "premiumtags.*";
+    public static final String MANAGE_TAGS = "premiumtags.tags.manage";
+}
